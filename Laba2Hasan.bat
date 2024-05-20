1c9a49d4e59fca29cdacd4c63c56ee167065e502
hasan140203 <167687277+hasan140203@users.noreply.github.com>
2024-05-20 19:39:53 +0300

    Create Laba2Hasan.bat

---
+++ Laba2Hasan.bat
@@ -0,0 +1,20 @@
+echo off
+md Tursunov
+cd Tursunov
+md Hasanxudza
+md Farruxovich
+pause
+echo > 14022003.txt
+cd Farruxovich
+echo > adb2107.txt
+pause
+cd ..
+cd ..
+del Tursunov /S /Q /F
+pause
+cd Tursunov
+rd Hasanxudza
+rd Farruxovich
+cd ..
+rd Tursunov
+pause
